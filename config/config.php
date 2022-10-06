8a3667b529889140b8b74fd3518f600487061072
ryn771 <99346414+ryn771@users.noreply.github.com>
2022-10-06 14:00:21 +0800

    added sql info

--- config/config.php
+++ config/config.php
@@ -1,6 +1,12 @@
 <?php
+// define('ROOT_URL', 'http://localhost/logApp-scaling-octo/index.php');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'test');
+
 define('ROOT_URL', 'http://localhost/logApp-scaling-octo/index.php');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'test');
+define('DB_HOST', 'sql6.freesqldatabase.com');
+define('DB_USER', 'sql6524604');
+define('DB_PASS', 'Zg5kj65P8T');
+define('DB_NAME', 'sql6524604');
