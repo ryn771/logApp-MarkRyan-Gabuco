109da521dac1c7bd18e6a84eb11a061b95ef2eba
ryn771 <99346414+ryn771@users.noreply.github.com>
2022-10-06 14:29:26 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ryn771/logapp-markryan_gabuco',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8a3667b529889140b8b74fd3518f600487061072',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ryn771/logapp-markryan_gabuco' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8a3667b529889140b8b74fd3518f600487061072',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
